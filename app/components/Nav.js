625a7b2ad85eebc210ce08c88eafcccffaf374fa
Tahir Iqbal Najam <tahiriqbal09@gmail.com>
2023-06-01 16:31:19 +0500

    links and images

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,61 +1,65 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from './images/logo.png';
+
 export default function Nav() {
     return (
-        <header class="main-header inner-header">
-            <div class="container-fluid">
-                <nav class="navbar navbar-expand-lg">
-                    <a class="navbar-brand" href="#">
-                        <img src="images/logo.png" alt="">
-                    </a>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    EXPLORA
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    AYUDA
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">COTIZA</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="header-profile">
-                        <div class="profile-box">
-                            <div class="dropdown">
-                                <div class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <span><img src="images/profile-img.png" alt=""></span> <p>BERNARDO GONZALEZ</p>  <i class="fa-solid fa-chevron-down"></i>
-                                </div>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                            <i class="fa-solid fa-bars"></i>
-                        </button>
-                    </div>
-                </nav>
-            </div>
-        </header>
+        <header className="main-header inner-header">
+		    <div className="container-fluid">
+			<nav className="navbar navbar-expand-lg">
+				<Link className="navbar-brand" href="#">
+					<Image src="/images/logo.png" alt="" width="64" height="64" />
+				</Link>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+						<li className="nav-item dropdown">
+							<Link className="nav-link dropdown-toggle" href="#" role="button"
+								data-bs-toggle="dropdown" aria-expanded="false">
+								EXPLORA
+							</Link>
+							<ul className="dropdown-menu">
+								<li><Link className="dropdown-item" href="#">Action</Link></li>
+								<li><Link className="dropdown-item" href="#">Another action</Link></li>
+								<li><Link className="dropdown-item" href="#">Something else here</Link></li>
+							</ul>
+						</li>
+						<li className="nav-item dropdown">
+							<Link className="nav-link dropdown-toggle" href="#" role="button"
+								data-bs-toggle="dropdown" aria-expanded="false">
+								AYUDA
+							</Link>
+							<ul className="dropdown-menu">
+								<li><Link className="dropdown-item" href="#">Action</Link></li>
+								<li><Link className="dropdown-item" href="#">Another action</Link></li>
+								<li><Link className="dropdown-item" href="#">Something else here</Link></li>
+							</ul>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" href="#">COTIZA</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="header-profile">
+					<div className="profile-box">
+						<div className="dropdown">
+							<div className="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+								<span><img src="images/profile-Image.png" alt="" /></span> <p>BERNARDO GONZALEZ</p>  <i className="fa-solid fa-chevron-down"></i>
+							</div>
+							<ul className="dropdown-menu">
+								<li><Link className="dropdown-item" href="#">Action</Link></li>
+								<li><Link className="dropdown-item" href="#">Another action</Link></li>
+								<li><Link className="dropdown-item" href="#">Something else here</Link></li>
+							</ul>
+						</div>
+					</div>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+						aria-expanded="false" aria-label="Toggle navigation">
+						<i className="fa-solid fa-bars"></i>
+					</button>
+				</div>
+			</nav>
+		    </div>
+	    </header>
     );
 }
