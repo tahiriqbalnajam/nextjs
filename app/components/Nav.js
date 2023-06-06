ec1014a943c61a98b322bcb8e83e2d4ae283f3fe
waqarnawaz007 <61963407+waqarnawaz007@users.noreply.github.com>
2023-06-05 22:35:29 -0500

    update

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -88,7 +88,7 @@ export default function Nav() {
 								</svg>
 							</button>
 							<div className="px-2 py-2 mt-2 bg-white rounded-md shadow-xs"
-								role="menu" aria-orientation="vertical" aria-labelledby="user-menu" role="menuitem">
+								role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
 								<a href="#"
 									className="flex flex-row items-center px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-200 focus:outline-none focus:text-gray-900 focus:bg-gray-200"
 									role="menuitem">
