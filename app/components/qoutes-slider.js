027fafea87a1b3bac9f3250b9c23ecbd026f5177
waqarnawaz007 <61963407+waqarnawaz007@users.noreply.github.com>
2023-06-07 12:08:34 -0500

    change

--- app/components/qoutes-slider.js
+++ app/components/qoutes-slider.js
@@ -1,58 +1,145 @@
 "use client"; 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 const images = [
   '/images/propiedades.png',
   '/images/barcos.png',
   '/images/salones.png',
+  '/images/terrazas.png',
+  "/images/tickets.png",
+  "/images/jardines.png",
+  "/images/hotels.png",
+  "/images/tickets.png",
 ];
 
 const QoutesImageSlider = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
-
   const previousImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
+  // Auto slide effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextImage();
+    }, 3000); // Set the desired interval time (e.g., 3000ms = 3 seconds)
+
+    return () => {
+      clearInterval(interval); // Clear the interval when the component unmounts
+    };
+  }, []);
 
   return (
-    <div className="mb-4">
-      <label htmlFor="">Presupuesto</label>
+    <div className="mb-10 mt-10">
+      <div className='myclass'>
+        <label className='text-left absolute' for="">Presupuesto</label>
+        <div className='text-right'>
+          <i className="fa-solid fa-chevron-left text-red-600" onClick={previousImage}></i>
+          <i class="fa-solid fa-chevron-right text-red-600 pl-3" onClick={nextImage}></i>
+        </div>
+      </div>
       <div className="banner-slider-wrapp mt-3">
         <div className="owl-carousel owl-theme service-slider">
-          <div className="flex items-center justify-center h-screen">
-            <i className="px-4 py-2 bg-gray-500 rounded-l" onClick={previousImage}></i>
-            <div className="item">
-              <div className="service-box">
+          <div className="flex items-center justify-center">
+            <div className="relative flex-grow max-w-[16%] flex-1 pr-4">
+              <div className="service-box px-5 py-5">
                 <Image
                   className="max-w-full h-auto"
                   src={images[currentImageIndex]}
                   alt={`Image ${currentImageIndex + 1}`}
-                  width={50}
-                  height={50}
+                  width={40}
+                  height={40}
                 />
-                <p>Propiedades</p>
+                <p className='text-[14px]'>Propiedades</p>
               </div>
             </div>
             {images.length > 2 && (
-              <div className="item">
-                <div className="service-box">
+              <div className="relative flex-grow max-w-[16%] flex-1 pr-4">
+                <div className="service-box px-5 py-5">
                   <Image
                     className="max-w-full h-auto"
                     src={images[(currentImageIndex + 1) % images.length]}
                     alt={`Image ${currentImageIndex + 2}`}
-                    width={50}
-                    height={50}
+                    width={40}
+                    height={40}
                   />
-                  <p>Barcos</p>
+                  <p className='text-[14px]'>Barcos</p>
                 </div>
               </div>
             )}
-            <i className="px-4 py-2 bg-gray-500 rounded-r" onClick={nextImage}></i>
+            {images.length > 3 && (
+              <div className="relative flex-grow max-w-[16%] flex-1 pr-4">
+                <div className="service-box px-5 py-5">
+                  <Image
+                    className="max-w-full h-auto"
+                    src={images[(currentImageIndex + 2) % images.length]}
+                    alt={`Image ${currentImageIndex + 3}`}
+                    width={40}
+                    height={40}
+                  />
+                  <p className='text-[14px]'>Barcos</p>
+                </div>
+              </div>
+            )}
+            {images.length > 4 && (
+              <div className="relative flex-grow max-w-[16%] flex-1 pr-4">
+                <div className="service-box px-5 py-5">
+                  <Image
+                    className="max-w-full h-auto"
+                    src={images[(currentImageIndex + 3) % images.length]}
+                    alt={`Image ${currentImageIndex + 4}`}
+                    width={40}
+                    height={40}
+                  />
+                  <p className='text-[14px]'>Barcos</p>
+                </div>
+              </div>
+            )}
+            {images.length > 5 && (
+              <div className="relative flex-grow max-w-[16%] flex-1 pr-4">
+                <div className="service-box px-5 py-5">
+                  <Image
+                    className="max-w-full h-auto"
+                    src={images[(currentImageIndex + 4) % images.length]}
+                    alt={`Image ${currentImageIndex + 5}`}
+                    width={40}
+                    height={40}
+                  />
+                  <p className='text-[14px]'>Barcos</p>
+                </div>
+              </div>
+            )}
+            {images.length > 6 && (
+              <div className="relative flex-grow max-w-[16%] flex-1 pr-4">
+                <div className="service-box px-5 py-5">
+                  <Image
+                    className="max-w-full h-auto"
+                    src={images[(currentImageIndex + 5) % images.length]}
+                    alt={`Image ${currentImageIndex + 6}`}
+                    width={40}
+                    height={40}
+                  />
+                  <p className='text-[14px]'>Barcos</p>
+                </div>
+              </div>
+            )}
+            {images.length > 7 && (
+              <div className="relative flex-grow max-w-[16%] flex-1 pr-4">
+                <div className="service-box px-5 py-5">
+                  <Image
+                    className="max-w-full h-auto"
+                    src={images[(currentImageIndex + 6) % images.length]}
+                    alt={`Image ${currentImageIndex + 7}`}
+                    width={40}
+                    height={40}
+                  />
+                  <p className='text-[14px]'>Barcos</p>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- app/myaccount/quotes/page.js
+++ app/myaccount/quotes/page.js
@@ -51,15 +51,15 @@ export default function Home() {
 							<div className="mb-4">
 								<label for="">Creemos que estos también te podrían interesar</label>
 								<div className="flex flex-wrap  mt-3 custom-col20">
-									<div className="relative flex-grow max-w-full flex-1 px-4">
+									<div className="relative flex-grow max-w-[16%] flex-1 px-4">
 										<div className="service-box">
-											<Image src="/images/propiedades.png" alt="" height='50' width='50' />
+											<Image src="/images/propiedades.png" alt="" height='40' width='40' />
 											<p>Propiedades</p>
 										</div>
 									</div>
-									<div className="relative flex-grow max-w-full flex-1 px-4">
+									<div className="relative flex-grow max-w-[16%] flex-1 px-4">
 										<div className="service-box">
-											<Image src="/images/propiedades.png" alt="" height='50' width='50' />
+											<Image src="/images/propiedades.png" alt="" height='40' width='40' />
 											<p>Propiedades</p>
 										</div>
 									</div>
@@ -96,131 +96,7 @@ export default function Home() {
 							</div>
 						</form>
 					</div>
-				</div>                    
-
-                <div className="main-content-area">
-					<div className="your-event mt-5 pt-5">
-						<div className="default-heading">
-							<h2><span>Cuéntanos</span> sobre tu evento</h2>
-						</div>
-						<form className="default-form border-fields">
-							<div className="form-group">
-								<label for="">Tipo de evento</label>
-								<Input type="password" className="form-control" placeholder="Boda, cumpleaños, empresarial, etc" />
-							</div>
-							<div className="form-group">
-								<label for="">No Aproximado de Asistentes</label>
-								<Input type="password" className="form-control" placeholder="" />
-							</div>
-							<div className="form-group">
-								<label for="">Presupuesto</label>
-								<Input type="password" className="form-control" placeholder="$0.00" />
-							</div>
-							<div className="form-group">
-								<label for="">Presupuesto</label>
-								<div className="banner-slider-wrapp mt-3">
-									<div className="owl-carousel owl-theme service-slider">
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/propiedades.png" alt="" height='50' width='50' />
-												<p>Propiedades</p>
-											</div>
-										</div>
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/barcos.png" alt="" height='50' width='50' />
-												<p>Barcos</p>
-											</div>
-										</div>
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/salones.png" alt="" height='50' width='50' />
-												<p>Salones de Fiesta</p>
-											</div>
-										</div>
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/terrazas.png" alt="" height='50' width='50' />
-												<p>Terrazas</p>
-											</div>
-										</div>
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/tickets.png" alt="" height='50' width='50' />
-												<p>Boletos</p>
-											</div>
-										</div>
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/jardines.png" alt="" height='50' width='50' />
-												<p>Jardines</p>
-											</div>
-										</div>
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/hotels.png" alt="" height='50' width='50' />
-												<p>Hoteles</p>
-											</div>
-										</div>
-										<div className="item">
-											<div className="service-box">
-												<Image src="/images/tickets.png" alt=""height='50' width='50' />
-												<p>Boletos</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="form-group">
-								<label for="">Creemos que estos también te podrían interesar</label>
-								<div className="row mt-3 custom-col20">
-									<div className="col">
-										<div className="service-box">
-											<Image src="/images/propiedades.png" alt="" height='50' width='50' />
-											<p>Propiedades</p>
-										</div>
-									</div>
-									<div className="col">
-										<div className="service-box">
-											<Image src="/images/propiedades.png" alt="" height='50' width='50' />
-											<p>Propiedades</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="form-group">
-								<label for="">Mensaje</label>
-								<textarea className="form-control" placeholder="Cualquier detalle nos será de utilidad"></textarea>
-							</div>
-							<div className="form-group">
-								<label for="">¿Cómo te contactamos?</label>
-								<div className="custom-radio-button-wrapp mt-3">
-									<div className="custom-radio-button">
-										<Input type="radio" id="Mail" name="radio-group" checked />
-										<label for="Mail">Mail</label>
-									</div>
-									<div className="custom-radio-button">
-										<Input type="radio" id="Llamada" name="radio-group" />
-										<label for="Llamada">Llamada</label>
-									</div>
-									<div className="custom-radio-button">
-										<Input type="radio" id="WhatsApp" name="radio-group" />
-										<label for="WhatsApp">WhatsApp</label>
-									</div>
-									<div className="custom-radio-button">
-										<Input type="radio" id="Todo" name="radio-group" />
-										<label for="Todo">Todo</label>
-									</div>
-								</div>
-							</div>
-							
-							<div className="form-group text-end">
-								<a href="#" className="btn-red">Enviar</a>
-							</div>
-						</form>
-					</div>
-				</div>
-                    
+				</div>                                        
                 </div>
             </div>
         </section>
