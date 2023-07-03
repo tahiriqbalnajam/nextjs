0d394b833f8e8ce4f2d9a1735910aa260fa12011
Tahir Iqbal Najam <tahiriqbal09@gmail.com>
2023-07-03 14:44:05 +0500

    cgange

--- app/components/qoutes-slider.js
+++ app/components/qoutes-slider.js
@@ -1,8 +1,30 @@
 "use client"; 
 import React, { useState, useEffect } from 'react';
+var $ = require('jquery');
+if (typeof window !== 'undefined') {
+   window.$ = window.jQuery = require('jquery');
+}
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import dynamic from 'next/dynamic';
+const OwlCarousel = dynamic(() => import('react-owl-carousel'), {
+  ssr: false,
+});
 import Image from 'next/image';
-
-
+const Responsive = {
+    0: {
+      items:7,
+      margin:5
+    },
+    786: {
+      items:7,
+      margin:10
+    },
+    1024: {
+      items:7,
+      margin:20
+    }
+};
 const images = [
   '/images/propiedades.png',
   '/images/barcos.png',
@@ -15,135 +37,58 @@ const images = [
 ];
 
 const QoutesImageSlider = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-  const previousImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
-  // Auto slide effect
-  useEffect(() => {
-    const interval = setInterval(() => {
-      nextImage();
-    }, 3000); // Set the desired interval time (e.g., 3000ms = 3 seconds)
-
-    return () => {
-      clearInterval(interval); // Clear the interval when the component unmounts
-    };
-  }, []);
-
   return (
     <div className="mb-10 mt-10">
-      <div className='myclass'>
-        <label className='text-left absolute' for="">Presupuesto</label>
-        <div className='text-right'>
-          <i className="fa-solid fa-chevron-left text-red-600" onClick={previousImage}></i>
-          <i class="fa-solid fa-chevron-right text-red-600 pl-3" onClick={nextImage}></i>
-        </div>
-      </div>
-      <div className="banner-slider-wrapp mt-3">
-        <div className="owl-carousel owl-theme service-slider">
-          <div className="flex items-center justify-center">
-            <div className="relative flex-grow max-w-[14%] flex-1 pr-4 pl-4 fimage">
-              <div className="service-box px-5 py-5">
-                <Image
-                  className="max-w-full h-auto"
-                  src={images[currentImageIndex]}
-                  alt={`Image ${currentImageIndex + 1}`}
-                  width={110}
-                  height={110}
-                />
-                <p className='text-[14px]'>Propiedades</p>
-              </div>
-            </div>
-            {images.length > 2 && (
-              <div className="relative flex-grow max-w-[14%] flex-1 pr-4 pl-4 fimage">
-                <div className="service-box px-5 py-5">
-                  <Image
-                    className="max-w-full h-auto"
-                    src={images[(currentImageIndex + 1) % images.length]}
-                    alt={`Image ${currentImageIndex + 2}`}
-                    width={110}
-                    height={110}
-                  />
-                  <p className='text-[14px]'>Barcos</p>
-                </div>
-              </div>
-            )}
-            {images.length > 3 && (
-              <div className="relative flex-grow max-w-[14%] flex-1 pr-4 pl-4 fimage">
-                <div className="service-box px-5 py-5">
-                  <Image
-                    className="max-w-full h-auto"
-                    src={images[(currentImageIndex + 2) % images.length]}
-                    alt={`Image ${currentImageIndex + 3}`}
-                    width={110}
-                    height={110}
-                  />
-                  <p className='text-[14px]'>Barcos</p>
-                </div>
-              </div>
-            )}
-            {images.length > 4 && (
-              <div className="relative flex-grow max-w-[14%] flex-1 pr-4 pl-4 fimage">
-                <div className="service-box px-5 py-5">
-                  <Image
-                    className="max-w-full h-auto"
-                    src={images[(currentImageIndex + 3) % images.length]}
-                    alt={`Image ${currentImageIndex + 4}`}
-                    width={110}
-                    height={110}
-                  />
-                  <p className='text-[14px]'>Barcos</p>
-                </div>
-              </div>
-            )}
-            {images.length > 5 && (
-              <div className="relative flex-grow max-w-[14%] flex-1 pr-4 pl-4 fimage">
-                <div className="service-box px-5 py-5">
-                  <Image
-                    className="max-w-full h-auto"
-                    src={images[(currentImageIndex + 4) % images.length]}
-                    alt={`Image ${currentImageIndex + 5}`}
-                    width={110}
-                    height={110}
-                  />
-                  <p className='text-[14px]'>Barcos</p>
-                </div>
-              </div>
-            )}
-            {images.length > 6 && (
-              <div className="relative flex-grow max-w-[14%] flex-1 pr-4 pl-4 fimage">
-                <div className="service-box px-5 py-5">
-                  <Image
-                    className="max-w-full h-auto"
-                    src={images[(currentImageIndex + 5) % images.length]}
-                    alt={`Image ${currentImageIndex + 6}`}
-                    width={110}
-                    height={110}
-                  />
-                  <p className='text-[14px]'>Barcos</p>
-                </div>
-              </div>
-            )}
-            {images.length > 7 && (
-              <div className="relative flex-grow max-w-[14%] flex-1 pr-4 pl-4 fimage lastimage">
-                <div className="service-box px-5 py-5">
-                  <Image
-                    className="max-w-full h-auto"
-                    src={images[(currentImageIndex + 6) % images.length]}
-                    alt={`Image ${currentImageIndex + 7}`}
-                    width={110}
-                    height={110}
-                  />
-                  <p className='text-[14px]'>Barcos</p>
-                </div>
-              </div>
-            )}
+      <OwlCarousel responsive={Responsive} className='owl-theme' autoplay={false} loop margin={10} nav={true} dots={false} navText={['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>']}> 
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/propiedades.png" alt=""  height="50" width="50" />
+            <p>Propiedades</p>
           </div>
         </div>
-      </div>
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/barcos.png" alt=""  height="50" width="50" />
+            <p>Barcos</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/salones.png" alt=""  height="50" width="50" />
+            <p>Salones de Fiesta</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/terrazas.png" alt=""  height="50" width="50" />
+            <p>Terrazas</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/tickets.png" alt=""  height="50" width="50" />
+            <p>Boletos</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/jardines.png" alt=""  height="50" width="50" />
+            <p>Jardines</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/hotels.png" alt=""  height="50" width="50" />
+            <p>Hoteles</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="service-box">
+            <Image src="/images/tickets.png" alt=""  height="50" width="50" />
+            <p>Boletos</p>
+          </div>
+        </div>
+      </OwlCarousel>
     </div>
   );
 };
