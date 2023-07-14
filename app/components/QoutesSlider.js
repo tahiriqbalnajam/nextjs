562be7b6c61fbff73ebecd0bafa5609b2b23d038
Tahir Iqbal Najam <tahiriqbal09@gmail.com>
2023-07-14 16:21:15 +0500

    quotesslire changes

--- app/components/QoutesSlider.js
+++ app/components/QoutesSlider.js
@@ -1,7 +1,6 @@
 "use client"; 
 
 import React, { useState, useEffect } from 'react';
-
 var $ = require('jquery');
 if (typeof window !== 'undefined') {
   window.$ = window.jQuery = require('jquery');
@@ -13,20 +12,6 @@ const OwlCarousel = dynamic(() => import('react-owl-carousel'), {
   ssr: false,
 });
 import Image from 'next/image';
-
-const QoutesImageSlider = () => {
-  const [selectedItems, setSelectedItems] = useState([]);
-
-  const handleItemClick = (index) => {
-    const itemIndex = selectedItems.indexOf(index);
-    if (itemIndex > -1) {
-      const updatedItems = [...selectedItems];
-      updatedItems.splice(itemIndex, 1);
-      setSelectedItems(updatedItems);
-    } else {
-      setSelectedItems([...selectedItems, index]);
-    }
-  };
 
 const Responsive = {
   0: {
@@ -43,89 +28,132 @@ const Responsive = {
   }
 };
 
-
 const image_text = [
   {
+    id: '1',
     url: '/images/propiedades.png',
     paragraph: 'propiedades',
   },
   {
+    id: '2',
     url: '/images/barcos.png',
     paragraph: 'Barcos',
   },
   {
+    id: '3',
     url: '/images/salones.png',
     paragraph: 'Salones de Fiesta',
   },
   {
+    id: '4',
     url: '/images/terrazas.png',
     paragraph: 'Terrazas',
   },
   {
+    id: '5',
     url: '/images/tickets.png',
     paragraph: 'Boletos',
   },
   {
+    id: '6',
     url: '/images/jardines.png',
     paragraph: 'Jardines',
   },
   {
+    id: '7',
     url: '/images/hotels.png',
     paragraph: 'Hoteles',
   },
   {
+    id: '8',
     url: '/images/tickets.png',
     paragraph: 'Boletos',
   },
 ];
 
-const list = image_text.map((item, index) => (
-  <div className={`item ${selectedItems.includes(index) ? 'selected' : ''}`} key={index} onClick={() => handleItemClick(index)}>
-    <div className="service-box">
-      <Image width={331} height={246} src={item.url} alt="" />
-      <p>{item.paragraph}</p>
-    </div>
-  </div>
-));
 
-console.log(selectedItems);
-const selectedItemsList = selectedItems.map((index) => (
-  <div className="relative flex-grow max-w-[108px] flex-1 pl-[12px]" key={index}>
-    <div className="service-box service-box2">
-      <Image width={331} height={246} src={image_text[index].url} alt="" />
-      <p>{image_text[index].paragraph}</p>
-    </div>
-  </div>
-));
+const navText = [
+  '<div className="text-[#ff5a5e] qoutesvg"><svg className="h-4 text-[#ff5a5e] dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/></svg>',
+  '<span><svg className="h-4 text-[#ff5a5e] dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/></svg></span></div>'
+];
 
-  const navText = [
-    '<div className="text-[#ff5a5e] qoutesvg"><svg className="h-4 text-[#ff5a5e] dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/></svg>',
-    '<span><svg className="h-4 text-[#ff5a5e] dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/></svg></span></div>'
-  ];
+export default class QoutesImageSlider extends React.Component {
+  constructor(args) {
+    super(args);
+    this.state = {
+      selectedItems: [],
+      selectedItemsList: []
+    }
+  }
 
-  return (
-    <>
-    <div className="mb-10 mt-10 sliderdiv">
-      
-      <OwlCarousel
-        responsive={Responsive}
-        className="owl-theme"
-        autoplay={false}
-        loop
-        margin={10}
-        nav={true}
-        dots={false}
-        navText={navText}
-      > 
-        {list}
-      </OwlCarousel>
-    </div>
-    <div className="mb-4">
-      <label>Creemos que estos también te podrían interesar</label>
-      <div className="flex flex-wrap mt-3 custom-col20">{selectedItemsList}</div>
-    </div>
-    </>
-  );
-};
+  componentDidMount() {
+  }
+  
+  handleItemClick(id) {
+    let someExist = this.state.selectedItemsList.some(item => item.id == id);
+  
+    if(someExist) {
+      console.log('item exists')
+      return;
+    }
+    
+    let selected = image_text.filter( item => item.id == id );
+    console.log(this.state.selectedItemsList);
+    this.setState((state, props) => ({
+      selectedItemsList: [...state.selectedItemsList, selected[0]]
+    }));
+  };
 
-export default QoutesImageSlider;
+  handleRemoveItem(id) {
+    let ListItems = this.state.selectedItemsList.filter(item => item.id != id);
+    this.setState((state, props) => ({
+      selectedItemsList: ListItems
+    }));
+
+  }
+  render() {
+      return (
+          <>
+          <div className="mb-10 mt-10 sliderdiv">
+              
+              <OwlCarousel
+              responsive={Responsive}
+              className="owl-theme"
+              autoplay={false}
+              loop
+              margin={10}
+              nav={true}
+              dots={false}
+              navText={navText}
+              > 
+              {
+                image_text.map((item, index) => (
+                  <div className='item' key={index} onClick={() => this.handleItemClick(item.id)}>
+                    <div className="service-box">
+                      <Image width={331} height={246} src={item.url} alt="" />
+                      <p>{item.paragraph}</p>
+                    </div>
+                  </div>
+                ))
+              }
+              </OwlCarousel>
+          </div>
+          <div className="mb-4">
+              <label>Creemos que estos también te podrían interesar</label>
+              <div className="flex flex-wrap mt-3 custom-col20">
+                {
+                  this.state.selectedItemsList.map((item, index) => (
+                    <div className="relative flex-grow max-w-[108px] flex-1 pl-[12px]" key={index} onClick={() => this.handleRemoveItem(item.id)}>
+                      <div className="service-box service-box2">
+                        <Image width={331} height={246} src={item.url} alt="" />
+                        <p>{item.paragraph}</p>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+          </div>
+          </>
+      )
+  };
+}
